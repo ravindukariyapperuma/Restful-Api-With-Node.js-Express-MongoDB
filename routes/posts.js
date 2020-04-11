7a9fde38fb2f0a79b5d44fd38c006d4d1dca18d8
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-12 01:29:07 +0530

    update title and discription

--- routes/posts.js
+++ routes/posts.js
@@ -52,7 +52,7 @@ router.patch('/:postId', async (req, res) => {
     try{
     const updatedPost = await Post.updateOne(
         { _id: req.params.postId },
-        { $set: { title: req.body.title }}
+        { $set: { title: req.body.title, description: req.body.description }}
         );
         res.json(updatedPost);
     }catch (err) {
