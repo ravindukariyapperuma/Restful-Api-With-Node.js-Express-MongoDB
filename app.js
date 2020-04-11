8b7244e97747f80dd6703a0e781f5f96b3f87e22
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 22:08:30 +0530

    bodyParser added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 //Import Routes
 const postsRoute = require('./routes/posts');
