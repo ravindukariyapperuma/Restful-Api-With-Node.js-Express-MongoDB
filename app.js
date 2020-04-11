ce984d5e82fc82cb5c67d95b2b845d24e8a7e98e
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 20:48:48 +0530

    middleware added

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+//Middlewares
+app.use('/posts', () => {
+    console.log('This is a middleware running');
+});
+
 //ROUTES
 app.get('/', (req, res) => {
     res.send('We are on home');
