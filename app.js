45b831dbe7baced91ee81245d122de19a6f6fa62
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 20:43:43 +0530

    nother route added

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ app.get('/', (req, res) => {
     res.send('We are on home');
 });
 
+app.get('/posts', (req, res) => {
+    res.send('We are on posts');
+});
+
 //How to we start listening to the server
 app.listen(3000);
