39233f4be52573c3bb0bc0a6b4792d85a144091e
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 22:30:39 +0530

    update get method

--- routes/posts.js
+++ routes/posts.js
@@ -2,23 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('We are on posts');
+router.get('/', async (req, res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({message:err});
+    }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
 
-    post.save()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
+    try{
+    const savedPost = await post.save();
+    res.json(savedPost);
+    }catch (err){
         res.json({ message: err });
-    });
+    }
 });
 
 module.exports = router;
